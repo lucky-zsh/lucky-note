75c55ace4a42ad234317c08cfa4c3262db7fad62
lucky <365873687@qq.com>
2023-10-23 00:05:43 +0800

    build docs

---
+++ rspress.config.ts
@@ -0,0 +1,25 @@
+import * as path from 'path';
+import { defineConfig } from 'rspress/config';
+
+export default defineConfig({
+  root: path.join(__dirname, 'docs'),
+  title: 'LuckyNote',
+  description: '深度学习前端知识',
+  icon: "/lucky-icon.png",
+  base:'/lucky-note/',
+  builderConfig:{
+    output: {
+      assetPrefix: '/',
+    }
+  },
+  mediumZoom:true,
+  logo: {
+    light: "/lucky-icon.png",
+    dark: "/lucky-icon.png",
+  },
+  themeConfig: {
+    socialLinks: [
+      { icon: 'github', mode: 'link', content: 'https://github.com/web-infra-dev/rspress' },
+    ],
+  },
+});
